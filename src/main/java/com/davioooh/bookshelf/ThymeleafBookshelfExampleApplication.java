36f5b3cc4e8d90d9df30e2e5475380b395b8c15d
David <davidcastelletti@yahoo.it>
2018-01-26 17:39:40 +0100

    Initial project configuration

---
+++ src/main/java/com/davioooh/bookshelf/ThymeleafBookshelfExampleApplication.java
@@ -0,0 +1,12 @@
+package com.davioooh.bookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafBookshelfExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafBookshelfExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/davioooh/bookshelf/ThymeleafBookshelfExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.davioooh.bookshelf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThymeleafBookshelfExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
