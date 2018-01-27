7973df0b89eaaa700834286710b250394638bc56
davioooh <davidcastelletti@yahoo.it>
2018-01-27 13:26:05 +0100

    Add BookController and MVC configuration

---
+++ src/main/java/com/davioooh/bookshelf/configurations/MVCConfiguration.java
@@ -0,0 +1,13 @@
+package com.davioooh.bookshelf.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MVCConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/books");
+    }
+}
---
+++ src/main/java/com/davioooh/bookshelf/controllers/BookController.java
@@ -0,0 +1,25 @@
+package com.davioooh.bookshelf.controllers;
+
+import com.davioooh.bookshelf.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/books")
+public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("")
+    public String showAllBooks(){
+        return "books";
+    }
+
+}
