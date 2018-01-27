e0819a230bd193b31cf1f3cda2294b1d0b34a789
davioooh <davidcastelletti@yahoo.it>
2018-01-27 12:53:37 +0100

    Add Lombok and some tests

--- src/main/java/com/davioooh/bookshelf/domain/Book.java
+++ src/main/java/com/davioooh/bookshelf/domain/Book.java
@@ -1,31 +1,14 @@
 package com.davioooh.bookshelf.domain;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Book {
     private Integer id;
     private String title;
+    private String author;
     private String description;
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 }
--- src/main/java/com/davioooh/bookshelf/repositories/ArrayListBookRepository.java
+++ src/main/java/com/davioooh/bookshelf/repositories/ArrayListBookRepository.java
@@ -2,8 +2,8 @@ package com.davioooh.bookshelf.repositories;
 
 import com.davioooh.bookshelf.domain.Book;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayListBookRepository implements BookRepository {
@@ -11,7 +11,12 @@ public class ArrayListBookRepository implements BookRepository {
     private List<Book> db;
 
     public ArrayListBookRepository() {
-        db = new ArrayList<>();
+        db = Lists.newArrayList(
+                Book.builder().id(1).title("Il vecchio e il mare").author("Ernest Hemingway").build(),
+                Book.builder().id(2).title("Il signore degli anelli").author("J. R. R. Tolkien").build(),
+                Book.builder().id(3).title("Il ritratto di Dorian Gray").author("Oscar Wilde").build()
+        );
+
         // TODO init db
     }
 
@@ -24,7 +29,7 @@ public class ArrayListBookRepository implements BookRepository {
     @Override
     public Book findOne(Integer id) {
         return db.stream()
-                .filter(b -> b.getId() == id)
+                .filter(b -> b.getId().equals(id))
                 .findFirst().orElse(null);
     }
 }
